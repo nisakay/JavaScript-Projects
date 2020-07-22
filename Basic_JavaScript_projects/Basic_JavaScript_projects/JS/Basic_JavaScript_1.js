4187fe4e9dcc96616b601a2d32cdd505c87be659
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2020-07-21 23:27:53 -0700

    Second Commit

---
+++ Basic_JavaScript_projects/Basic_JavaScript_projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+window.alert ("Hello, world!");
+document.write("\"samsung and," + "mercedes\"");
+var Car="Mercedes", Phone="Samsung";
+var Car= Car.fontcolor("blue");
+var Phone= Phone.fontcolor("blue");
+document.write(Phone);    
+document.write(3+3); //This code should generate the result of 3+3
