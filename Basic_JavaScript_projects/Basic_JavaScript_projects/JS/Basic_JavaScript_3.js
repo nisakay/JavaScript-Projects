80ec80e61598871ceb32e9fa4eb8f082ab36e7fc
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2020-07-27 18:18:45 -0700

    Third commit

---
+++ Basic_JavaScript_projects/Basic_JavaScript_projects/JS/Basic_JavaScript_3.js
@@ -0,0 +1,16 @@
+function more_Math() {
+    var simple_Math = (2+4) * 10 / 2 -5;
+    document.getElementById("Math").innerHTML = "2 plus 4, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
+    var X = 35;
+    X++;
+    document.write(X)
+}
+
+function modulus_Operator() {
+    var simple_Math = 20 % 5;
+    document.getElementById("Math_2").innerHTML = "When you divide 20 by 5 you have a remainder of:" + simple_Math;
+    var X = 5;
+    X--;
+    document.write(X)
+    window.alert(Math.random() * 100);
+}
