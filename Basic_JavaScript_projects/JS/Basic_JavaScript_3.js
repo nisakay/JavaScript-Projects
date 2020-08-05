4e307df430a930c23ca06918b60ca13058a86c13
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2020-08-05 02:04:46 -0700

    Math.random()

--- Basic_JavaScript_projects/JS/Basic_JavaScript_3.js
+++ Basic_JavaScript_projects/JS/Basic_JavaScript_3.js
@@ -1,14 +1,26 @@
-function more_Math() {
-    var simple_Math = (2+4) * 10 / 2 -5;
-    document.getElementById("Math").innerHTML = "2 plus 4, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
-    var X = 35;
-    X++;
-    document.write(X)
+function addition_Function() {
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML = "2 + 2 =" +addition;
+}
+
+function subtraction_Function() {
+    var Subtraction = 5 - 2;
+    document.getElementById("Math_2").innerHTML = "5 - 2 =" +Subtraction;
+}
+
+function multiplication_Function() {
+    var simple_Math = 6 * 8;
+    document.getElementById("Math_3").innerHTML = "6 * 8 =" +simple_Math;
+}
+
+function division_Function() {
+    var simple_Math = 48 / 6;
+    document.getElementById("Math_4").innerHTML = "48 / 6 =" +simple_Math;
 }
 
 function modulus_Operator() {
     var simple_Math = 20 % 5;
-    document.getElementById("Math_2").innerHTML = "When you divide 20 by 5 you have a remainder of:" + simple_Math;
+    document.getElementById("Math_5").innerHTML = "When you divide 20 by 5 you have a remainder of:" + simple_Math;
     var X = 5;
     X--;
     document.write(X)
